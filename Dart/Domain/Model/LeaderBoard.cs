920927519c55c864bad7798a6cadefb83e31a788
navidbigdeli54 <navidbigdeli54@gmail.com>
2022-06-22 00:31:11 +0430

    added Domain project added Leaderboard and Player classes

---
+++ Dart/Domain/Model/LeaderBoard.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Model
+{
+    public class LeaderBoard
+    {
+        #region Properties
+        public Player Player { get; set; }
+
+        public int Rank { get; set; }
+        #endregion
+    }
+}
---
+++ Dart/Domain/Model/Player.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Model
+{
+    public class Player
+    {
+        #region Properties
+        public Guid Id { get; set; }
+
+        public int Room { get; set; }
+
+        public int Score { get; set; }
+        #endregion
+    }
+}
