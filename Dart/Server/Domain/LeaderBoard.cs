5c983fd52307358cbd2867d66a5ec75f0ee40211
navidbigdeli54 <navidbigdeli54@gmail.com>
2022-06-22 23:13:00 +0430

    added leaderboard and player classes

---
+++ Dart/Server/Domain/LeaderBoard.cs
@@ -0,0 +1,11 @@
+﻿namespace Server.Domain
+{
+    public class LeaderBoard
+    {
+        #region Properties
+        public Player Player { get; set; }
+
+        public int Rank { get; set; }
+        #endregion
+    }
+}
---
+++ Dart/Server/Domain/Player.cs
@@ -0,0 +1,17 @@
+﻿namespace Server.Domain
+{
+    public class Player
+    {
+        #region Properties
+        public Guid Id { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public int Room { get; set; }
+
+        public List<int> TurnScores { get; set; } = new List<int>();
+
+        public int TotalScore { get; set; }
+        #endregion
+    }
+}
