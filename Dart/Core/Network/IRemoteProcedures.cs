d03159cc5902441e6039a8cb6339fd35e61b5a34
navidbigdeli54 <navidbigdeli54@gmail.com>
2022-06-23 02:00:31 +0430

    added RemoteProcedure class that will be used to map socket evemt to methods (Something similar to the SignalR's Hub)

---
+++ Dart/Core/Network/IRemoteProcedures.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+
+namespace Network
+{
+    public interface IRemoteProcedures
+    {
+        void Invoke(string method);
+
+        void Invoke(string method, object[] parameters);
+    }
+
+    public abstract class RemoteProcedures : IRemoteProcedures
+    {
+        #region Fields
+        private readonly Dictionary<string, MethodInfo> _procedures = new Dictionary<string, MethodInfo>();
+        #endregion
+
+        #region Protected Methods
+        public RemoteProcedures()
+        {
+            Type type = GetType();
+
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+            for (int i = 0; i < methods.Length; ++i)
+            {
+                MethodInfo methodInfo = methods[i];
+                _procedures.Add(methodInfo.Name, methodInfo);
+            }
+        }
+        #endregion
+
+        #region IRemoteProcedures Implementation
+        void IRemoteProcedures.Invoke(string method)
+        {
+            ((IRemoteProcedures)this).Invoke(method, null);
+        }
+
+        void IRemoteProcedures.Invoke(string method, object[] parameters)
+        {
+            if (_procedures.TryGetValue(method, out MethodInfo methodInfo))
+            {
+                methodInfo?.Invoke(this, parameters);
+            }
+        }
+        #endregion
+    }
+}
---
+++ Dart/Test/Test.Network/RemoteProcedureTest.cs
@@ -0,0 +1,119 @@
+﻿using Network;
+
+namespace Test.Network
+{
+    public class RemoteProcedureTest
+    {
+        #region Public Methods
+        [Test]
+        public void InvokingParameterlessProcedureTest()
+        {
+            DummyRemoteParameterlessProcedures remoteProcedures = new DummyRemoteParameterlessProcedures();
+
+            ((IRemoteProcedures)remoteProcedures).Invoke("DummyProcedure");
+
+            Assert.IsTrue(remoteProcedures.IsCalled);
+        }
+
+        [Test]
+        public void InvokingSingleParameterProcedureTest()
+        {
+            DummyRemoteSingleParameterProcedures remoteProcedures = new DummyRemoteSingleParameterProcedures();
+
+            string parameter = Guid.NewGuid().ToString();
+            ((IRemoteProcedures)remoteProcedures).Invoke("DummyProcedure", new object[] { parameter });
+
+            Assert.IsTrue(remoteProcedures.IsCalled);
+            Assert.AreEqual(parameter, remoteProcedures.Parameter);
+        }
+
+        [Test]
+        public void InvokingTwoeParameterProcedureTest()
+        {
+            DummyRemoteTwoDifferentParameterProcedures remoteProcedures = new DummyRemoteTwoDifferentParameterProcedures();
+
+            int firstParameter = Random.Shared.Next(0, int.MaxValue);
+            bool secondParameter = Random.Shared.Next(0, 1) % 2 == 0;
+            ((IRemoteProcedures)remoteProcedures).Invoke("DummyProcedure", new object[] { firstParameter, secondParameter });
+
+            Assert.IsTrue(remoteProcedures.IsCalled);
+            Assert.AreEqual(firstParameter, remoteProcedures.FirstParameter);
+            Assert.AreEqual(secondParameter, remoteProcedures.SecondParameter);
+        }
+
+        [Test]
+        public void ConstructingTwoRemoteProcedureWithTheSameNameTest()
+        {
+            Assert.Throws<ArgumentException>(() => new DummyTwoRemoteProcedureWithTheSameName());
+        }
+
+        [Test]
+        public void InvokingPrivateProcedureTest()
+        {
+            DummyRemoteProcedurePrivateMethod remoteProcedures = new DummyRemoteProcedurePrivateMethod();
+
+            ((IRemoteProcedures)remoteProcedures).Invoke("DummyProcedure");
+
+            Assert.IsFalse(remoteProcedures.IsCalled);
+        }
+        #endregion
+
+        #region Nested Types
+        private class DummyRemoteParameterlessProcedures : RemoteProcedures
+        {
+            public bool IsCalled { get; private set; }
+
+            public void DummyProcedure()
+            {
+                IsCalled = true;
+            }
+        }
+
+        private class DummyRemoteSingleParameterProcedures : RemoteProcedures
+        {
+            public bool IsCalled { get; private set; }
+
+            public string Parameter { get; private set; }
+
+            public void DummyProcedure(string parameter)
+            {
+                IsCalled = true;
+                Parameter = parameter;
+            }
+        }
+
+        private class DummyRemoteTwoDifferentParameterProcedures : RemoteProcedures
+        {
+            public bool IsCalled { get; private set; }
+
+            public int FirstParameter { get; private set; }
+
+            public bool SecondParameter { get; private set; }
+
+            public void DummyProcedure(int first, bool second)
+            {
+                IsCalled = true;
+                FirstParameter = first;
+                SecondParameter = second;
+            }
+        }
+
+        private class DummyTwoRemoteProcedureWithTheSameName : RemoteProcedures
+        {
+            public void DummyProcedure() { }
+
+            public void DummyProcedure(string parameter) { }
+        }
+
+        private class DummyRemoteProcedurePrivateMethod : RemoteProcedures
+        {
+            public bool IsCalled { get; private set; }
+
+            private void DummyProcedure()
+            {
+                IsCalled = true;
+            }
+        }
+        #endregion
+    }
+}
