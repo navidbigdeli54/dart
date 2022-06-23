1f6ad3c383431c6d5d6c8b52f763c6d655c67ed7
navidbigdeli54 <navidbigdeli54@gmail.com>
2022-06-24 01:34:20 +0430

    added game class in client to control game rules

--- Dart/App/Client/ClientRemoteProcedures.cs
+++ Dart/App/Client/ClientRemoteProcedures.cs
@@ -4,25 +4,13 @@ namespace Client
 {
     public class ClientRemoteProcedures : RemoteProcedures
     {
+        private Game _game;
+
         public void Connected(string userId)
         {
-            Console.WriteLine($"UserId is {userId}");
+            _game = new Game(Guid.Parse(userId));
 
-            int[] scores = new int[] { 1, 2, 3, 5, 10, 15, 25 };
-
-            for (int i = 0; i < 10; ++i)
-            {
-                Procedure procedure = new Procedure(
-                    "DartThrowed",
-                    new Parameter[] {
-                        new Parameter("userId", userId),
-                        new Parameter("Score", scores[Random.Shared.Next(0, scores.Length)])
-                    });
-
-                Program.ClientInstance.Send(procedure);
-
-                Thread.Sleep(1000);
-            }
+            _game.Start();
         }
 
         public void UpdateLeaderboard(string strigifinedJson)
---
+++ Dart/App/Client/Game.cs
@@ -0,0 +1,62 @@
+﻿using Network;
+
+namespace Client
+{
+    public class Game
+    {
+        private const float HIT_CHANCE = 0.6f;
+
+        private const int MISS_SCORE = -5;
+
+        private readonly int[] HIT_SCORE = new int[] { 1, 2, 3, 5, 10, 15, 25 };
+
+        private readonly int MAX_THROW = 10;
+
+        private readonly int THROW_INTERVAL = 5000;
+
+        private readonly Guid _userId;
+
+        public Game(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public void Start()
+        {
+            for (int i = 0; i < MAX_THROW; ++i)
+            {
+                Throw();
+
+                Thread.Sleep(THROW_INTERVAL);
+            }
+        }
+
+        private void Throw()
+        {
+            int currentScore = CalculateScore();
+
+            SendToServer(currentScore);
+        }
+
+        private int CalculateScore()
+        {
+            float dice = Random.Shared.NextSingle();
+
+            if (dice < HIT_CHANCE) return HIT_SCORE[Random.Shared.Next(0, HIT_SCORE.Length)];
+
+            else return MISS_SCORE;
+        }
+
+        private void SendToServer(int score)
+        {
+            Procedure procedure = new Procedure(
+                    "DartThrowed",
+                    new Parameter[] {
+                        new Parameter("userId", _userId),
+                        new Parameter("Score", score)
+                    });
+
+            Program.ClientInstance.Send(procedure);
+        }
+    }
+}
