362286a8d609d514da500b1090d9e7ddcd785ef6
navidbigdeli54 <navidbigdeli54@gmail.com>
2022-06-24 13:05:13 +0430

    addedd LeaderboardEntryPredicate class

---
+++ Dart/App/Server/Domain/LeaderboardEntryPredicate.cs
@@ -0,0 +1,10 @@
+﻿namespace Server.Domain
+{
+    public class LeaderboardEntryPredicate
+    {
+        public static Predicate<LeaderBoardEntry> FindUpperRank(LeaderBoardEntry entry)
+        {
+            return x => entry.Score <= x.Score && entry.GameSeason?.User.Username.CompareTo(x.GameSeason?.User.Username) > 0;
+        }
+    }
+}
