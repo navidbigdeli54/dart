876abfade7f466abc4b170f8d8354523a0ecae09
navidbigdeli54 <navidbigdeli54@gmail.com>
2022-06-24 23:48:18 +0430

    added Payload to the stateObject to keep track of it

--- Dart/Core/Network/Payload.cs
+++ Dart/Core/Network/Payload.cs
@@ -3,20 +3,23 @@ using System.Text.Json.Nodes;
 
 namespace Network
 {
-    public struct Payload
+    public class Payload
     {
         #region Fields
-        public static readonly int PAYLOAD_SIZE = Encoding.ASCII.GetBytes(new Payload(StateObject.BUFFER_SIZE).ToJson().ToJsonString()).Length; 
+        public static readonly int PAYLOAD_SIZE = Encoding.ASCII.GetBytes(new Payload(StateObject.BUFFER_SIZE).ToJson().ToJsonString()).Length;
         #endregion
 
         #region Properties
         public int Lenght { get; }
+
+        public int RemainingLenght { get; set; }
         #endregion
 
         #region Constructors
         public Payload(int lenght)
         {
             Lenght = lenght;
+            RemainingLenght = Lenght;
         }
 
         public Payload(JsonObject jsonObject)
--- Dart/Core/Network/StateObject.cs
+++ Dart/Core/Network/StateObject.cs
@@ -1,17 +1,22 @@
 ﻿using System.Net.Sockets;
+using System.Text;
 
 namespace Network
 {
     public class StateObject
     {
         #region Fields
-        public const int BUFFER_SIZE = short.MaxValue;
+        public const int BUFFER_SIZE = 1024;
         #endregion
 
         #region Properties
         public byte[] Buffer { get; } = new byte[BUFFER_SIZE];
 
         public Socket Socket { get; }
+
+        public StringBuilder StringBuilder { get; } = new StringBuilder();
+
+        public Payload Payload { get; set; }
         #endregion
 
         public StateObject(Socket socket)
