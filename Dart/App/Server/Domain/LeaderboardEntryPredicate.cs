c282dc8386c8ed3beb4cc1af61093a68d08787eb
navidbigdeli54 <navidbigdeli54@gmail.com>
2022-06-24 13:24:56 +0430

    fixed findUpperRank predicate

--- Dart/App/Server/Domain/LeaderboardEntryPredicate.cs
+++ Dart/App/Server/Domain/LeaderboardEntryPredicate.cs
@@ -4,7 +4,8 @@
     {
         public static Predicate<LeaderBoardEntry> FindUpperRank(LeaderBoardEntry entry)
         {
-            return x => entry.Score <= x.Score && entry.GameSeason?.User.Username.CompareTo(x.GameSeason?.User.Username) > 0;
+            return x => entry.Score < x.Score
+                || (entry.Score == x.Score && entry.GameSeason?.User.Username.CompareTo(x.GameSeason?.User.Username) > 0);
         }
     }
 }
