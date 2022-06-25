4688dffb4b620a0057f649cd512096358762bf02
Navid Bigdeli <navidbigdeli54@gmail.com>
2022-06-25 15:07:52 +0430

    added ef project

---
+++ Dart/Core/EF/EFContext.cs
@@ -0,0 +1,41 @@
+using Domain.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.EF
+{
+    public class EFContext : DbContext
+    {
+        #region Properties
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<GameSeason> GameSeasons { get; set; }
+
+        public DbSet<LeaderboardEntry> Leaderboard { get; set; }
+        #endregion
+
+        #region Protected Methods
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Database=DartDB;Username=postgres");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<GameSeason>()
+                .HasOne<User>()
+                .WithOne()
+                .HasForeignKey<GameSeason>(x=>x.UserId)
+                .IsRequired();
+
+            modelBuilder.Entity<LeaderboardEntry>()
+                .HasOne<GameSeason>()
+                .WithOne()
+                .HasForeignKey<LeaderboardEntry>(x=>x.GameSeasonId)
+                .IsRequired();
+        }
+        #endregion
+    }
+}
