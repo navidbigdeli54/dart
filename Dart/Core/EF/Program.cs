d40a6f6db636384380e22c199d89f5b69ec91d30
Navid Bigdeli <navidbigdeli54@gmail.com>
2022-06-25 15:08:08 +0430

    remaining of previous commit

---
+++ Dart/Core/EF/Program.cs
@@ -0,0 +1,13 @@
+﻿using Core.EF;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace EF
+{
+    public class Program
+    {
+        static void Main()
+        {
+            EFContext context = new EFContext();
+        }
+    }
+}
