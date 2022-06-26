1b2f5283bf0fc58834c0651ae325b4e31dc203f1
navidbigdeli54 <navidbigdeli54@gmail.com>
2022-06-26 23:19:05 +0430

    trash code to send top 3 person of the leaderboard when new client connect to the server

--- Dart/App/App.Server/Application/ApplicationRemoteProcedures.cs
+++ Dart/App/App.Server/Application/ApplicationRemoteProcedures.cs
@@ -19,25 +19,51 @@ namespace App.Server.Application
         public void RegisterUser(string username, string remoteEndPoint)
         {
             UserBL userBL = new UserBL(Program.ApplicationContext);
+            LeaderboadBL leaderboadBL = new LeaderboadBL(Program.ApplicationContext);
+            GameSeasonBL gameSeasonBL = new GameSeasonBL(Program.ApplicationContext);
+
+            IPEndPoint clientEndPoint = IPEndPoint.Parse(remoteEndPoint);
+
             IResult<Guid> addUserResult = userBL.Add(username, remoteEndPoint);
             if (addUserResult.IsSuccessful)
             {
-                GameSeasonBL gameSeasonBL = new GameSeasonBL(Program.ApplicationContext);
                 IResult<Guid> gameSeasonResult = gameSeasonBL.Add(addUserResult.Message);
                 if (gameSeasonResult.IsSuccessful)
                 {
-                    LeaderboadBL leaderboadBL = new LeaderboadBL(Program.ApplicationContext);
                     IResult addLeaderboardResult = leaderboadBL.Add(gameSeasonResult.Message);
                     if (addLeaderboardResult.IsSuccessful)
                     {
-                        IPEndPoint clientEndPoint = IPEndPoint.Parse(remoteEndPoint);
+                        Procedure userRegisteredProcedure = new Procedure("UserRegistered", new Parameter[] { new Parameter("userId", addUserResult.Message.ToString()) });
 
-                        Procedure procedure = new Procedure("UserRegistered", new Parameter[] { new Parameter("userId", addUserResult.Message.ToString()) });
-
-                        Program.ServerInstance.Send(clientEndPoint, procedure);
+                        Program.ServerInstance.Send(clientEndPoint, userRegisteredProcedure);
                     }
                 }
             }
+
+            List<ImmutableUserLeaderboard> leaderboardEntries = new List<ImmutableUserLeaderboard>();
+            IReadOnlyList<ImmutableLeaderboard> top3 = leaderboadBL.Get(3);
+            for (int i = 0; i < top3.Count; ++i)
+            {
+                ImmutableLeaderboard leaderBoardEntry = top3[i];
+
+                ImmutableGameSeason gameSeason = gameSeasonBL.Get(leaderBoardEntry.GameSeasonId);
+
+                ImmutableUser user = userBL.Get(gameSeason.UserId);
+
+                leaderboardEntries.Add(new ImmutableUserLeaderboard(user, leaderBoardEntry));
+            }
+
+            JsonObject jsonObject = new JsonObject();
+            JsonArray leaderboardArray = new JsonArray();
+            for (int i = 0; i < leaderboardEntries.Count; ++i)
+            {
+                leaderboardArray.Add(leaderboardEntries[i].ToJson());
+            }
+            jsonObject["Leaderboard"] = leaderboardArray;
+
+            Procedure procedure = new Procedure("UpdateLeaderboard", new Parameter[] { new Parameter("leaderboard", jsonObject.ToJsonString()) });
+
+            Program.ServerInstance.Send(clientEndPoint, procedure);
         }
 
         public void DartThrowed(Guid userId, int score)
