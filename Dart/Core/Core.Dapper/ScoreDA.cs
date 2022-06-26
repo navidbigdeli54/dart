4bbd1b4074ca6db5acea2ab6af0171ea1f24ae3b
Navid Bigdeli <navidbigdeli54@gmail.com>
2022-06-26 12:48:15 +0430

    added ScoreDA

---
+++ Dart/Core/Core.Dapper/ScoreDA.cs
@@ -0,0 +1,74 @@
+using Dapper;
+using System.Data;
+using Core.Domain.Core;
+using Core.Domain.Model;
+
+namespace Core.Dapper
+{
+    public class ScoreDA : BaseDA
+    {
+        #region Fields
+        private const string TABLE_NAME = "public.\"tblScore\"";
+
+        private IApplicationContext _applicationContext;
+        #endregion
+
+        #region Constructors
+        public ScoreDA(IApplicationContext applicationContext)
+        {
+            _applicationContext = applicationContext;
+        }
+        #endregion
+
+        #region Public Methods
+        public Score Get(int id)
+        {
+            string query = $"SELECT * FROM {TABLE_NAME} where \"{nameof(Score.Id)}\" = @{nameof(Score.Id)};";
+
+            using (IDbConnection connection = OpenConnection(_applicationContext.DBConnectionString))
+            {
+                return connection.QuerySingleOrDefault<Score>(query, new { Id = id });
+            }
+        }
+
+        /*
+            TODO:
+            Paging needed here!
+        */
+        public Score GetAll()
+        {
+            string query = $"SELECT * FROM {TABLE_NAME};";
+
+            using (IDbConnection connection = OpenConnection(_applicationContext.DBConnectionString))
+            {
+                return connection.QuerySingleOrDefault<Score>(query);
+            }
+        }
+
+        public IResult Add(Score score)
+        {
+            string query = $"INSERT INTO {TABLE_NAME} (\"{nameof(Score.Id)}\", \"{nameof(Score.GameSeasonId)}\", \"{nameof(Score.Point)}\") VALUES (@{nameof(Score.Id)}, @{nameof(Score.GameSeasonId)}, @{nameof(Score.Point)});";
+
+            using (IDbConnection connection = OpenConnection(_applicationContext.DBConnectionString))
+            {
+                IDbTransaction transaction = connection.BeginTransaction();
+                try
+                {
+                    connection.Query(query, new { Id = score.Id, GameSeasonId = score.GameSeasonId, Point = score.Point });
+
+                    transaction.Commit();
+
+                    return new Result<object>();
+
+                }
+                catch (Exception exception)
+                {
+                    transaction.Rollback();
+                    Console.WriteLine(exception);
+                    return new ErrorResult<object>(new List<string> { "Can't add score", exception.ToString() });
+                }
+            }
+        }
+        #endregion
+    }
+}
