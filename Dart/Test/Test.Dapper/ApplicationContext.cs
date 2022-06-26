6d6ea65c9c7c2de1f05b5df712130f5beb1f097e
Navid Bigdeli <navidbigdeli54@gmail.com>
2022-06-26 15:57:31 +0430

    added ApplicationContext in the Test.Dapper project

---
+++ Dart/Test/Test.Dapper/ApplicationContext.cs
@@ -0,0 +1,14 @@
+using Core.Domain.Core;
+
+
+namespace Test.Dapper
+{
+    public class ApplicationContext : IApplicationContext
+    {
+        #region IApplicationContext Implementation
+        string IApplicationContext.DBConnectionString { get; } = "Host=localhost;Database=DartDBTest;Username=postgres;Password=abcd1234";
+
+        ApplicationCache IApplicationContext.ApplicationCache { get; } = null;
+        #endregion
+    }
+}
