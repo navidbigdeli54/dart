8bbc609905cbd7ee1333cb470185ed8c9c9f1ef3
Navid Bigdeli <navidbigdeli54@gmail.com>
2022-06-26 12:42:12 +0430

    added GameSeasonDAL

---
+++ Dart/Core/Core.Dapper/GameSeasonDAL.cs
@@ -0,0 +1,70 @@
+using Dapper;
+using System.Data;
+using Core.Domain.Core;
+using Core.Domain.Model;
+
+namespace Core.Dapper
+{
+    public class GameSeasonDAL : BaseDA
+    {
+        #region Fields
+        private const string TABLE_NAME = "public.\"tblGameSeason\"";
+
+        private IApplicationContext _applicationContext;
+        #endregion
+
+        #region Constructors
+        public GameSeasonDAL(IApplicationContext applicationContext)
+        {
+            _applicationContext = applicationContext;
+        }
+        #endregion
+
+        #region Public Methods
+        public GameSeason Get(int id)
+        {
+            string query = $"SELECT * FROM {TABLE_NAME} where \"{nameof(GameSeason.Id)}\" = @{nameof(GameSeason.Id)};";
+
+            using (IDbConnection connection = OpenConnection(_applicationContext.DBConnectionString))
+            {
+                return connection.QuerySingleOrDefault<GameSeason>(query, new { Id = id });
+            }
+        }
+
+        public GameSeason GetAll()
+        {
+            string query = $"SELECT * FROM {TABLE_NAME};";
+
+            using (IDbConnection connection = OpenConnection(_applicationContext.DBConnectionString))
+            {
+                return connection.QuerySingleOrDefault<GameSeason>(query);
+            }
+        }
+
+        public IResult Add(GameSeason gameSeason)
+        {
+            string query = $"INSERT INTO {TABLE_NAME} (\"{nameof(GameSeason.Id)}\", \"{nameof(GameSeason.CreationDate)}\", \"{nameof(GameSeason.UserId)}\") VALUES (@{nameof(GameSeason.Id)}, @{nameof(GameSeason.CreationDate)}, @{nameof(GameSeason.UserId)});";
+
+            using (IDbConnection connection = OpenConnection(_applicationContext.DBConnectionString))
+            {
+                IDbTransaction transaction = connection.BeginTransaction();
+                try
+                {
+                    connection.Query(query, new { Id = gameSeason.Id, CreationDate = gameSeason.CreationDate, UserId = gameSeason.UserId });
+
+                    transaction.Commit();
+
+                    return new Result<object>();
+
+                }
+                catch (Exception exception)
+                {
+                    transaction.Rollback();
+                    Console.WriteLine(exception);
+                    return new ErrorResult<object>(new List<string> { "Can't add game season", exception.ToString() });
+                }
+            }
+        }
+        #endregion
+    }
+}
