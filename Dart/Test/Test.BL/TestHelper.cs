129eb47dd95a3d8888a258e5add176c65a29c5b6
Navid Bigdeli <navidbigdeli54@gmail.com>
2022-06-27 11:21:11 +0430

    added TestHelper into the Test.BL project

---
+++ Dart/Test/Test.BL/TestHelper.cs
@@ -0,0 +1,36 @@
+using Core.BL;
+using Core.Domain.Core;
+using Core.Domain.Model;
+
+namespace Test.BL
+{
+    public static class TestHelper
+    {
+        public static ImmutableUser AddUser(IApplicationContext applicationContext, string username, string endPoint)
+        {
+            UserBL userBL = new UserBL(applicationContext);
+            IResult<Guid> result = userBL.Add(Guid.NewGuid().ToString(), "[::1]:100");
+            Assert.That(result.IsSuccessful, Is.True);
+
+            return userBL.Get(result.Message);
+        }
+
+        public static ImmutableGameSeason AddGameSeason(IApplicationContext applicationContext, ImmutableUser user)
+        {
+            GameSeasonBL gameSeasonBL = new GameSeasonBL(applicationContext);
+            IResult<Guid> result = gameSeasonBL.Add(user.Id);
+            Assert.That(result.IsSuccessful, Is.True);
+
+            return gameSeasonBL.Get(result.Message);
+        }
+
+        public static ImmutableLeaderboard AddLeaderboard(IApplicationContext applicationContext, ImmutableGameSeason gameSeason)
+        {
+            LeaderboadBL leaderboardBL = new LeaderboadBL(applicationContext);
+            IResult<Guid> result = leaderboardBL.Add(gameSeason.Id);
+            Assert.That(result.IsSuccessful, Is.True);
+
+            return leaderboardBL.Get(result.Message);
+        }
+    }
+}
