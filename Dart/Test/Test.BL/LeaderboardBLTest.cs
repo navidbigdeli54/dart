8498ba1544c6757cdff54782e7658a411ac6260a
Navid Bigdeli <navidbigdeli54@gmail.com>
2022-06-27 11:29:26 +0430

    added a test for LeaderbaordBL.Get(count)

--- Dart/Test/Test.BL/LeaderboardBLTest.cs
+++ Dart/Test/Test.BL/LeaderboardBLTest.cs
@@ -28,6 +28,45 @@ namespace Test.BL
         }
 
         [Test]
+        public void GetSomeEntriesTest()
+        {
+            ApplicationContext applicationContext = new ApplicationContext();
+
+            ImmutableUser user1 = TestHelper.AddUser(applicationContext, string.Empty, "[::1]:100");
+            ImmutableGameSeason gameSeason1 = TestHelper.AddGameSeason(applicationContext, user1);
+            ImmutableLeaderboard leaderboard1 = TestHelper.AddLeaderboard(applicationContext, gameSeason1);
+
+            ImmutableUser user2 = TestHelper.AddUser(applicationContext, string.Empty, "[::1]:101");
+            ImmutableGameSeason gameSeason2 = TestHelper.AddGameSeason(applicationContext, user2);
+            ImmutableLeaderboard leaderboard2 = TestHelper.AddLeaderboard(applicationContext, gameSeason2);
+
+            ImmutableUser user3 = TestHelper.AddUser(applicationContext, string.Empty, "[::1]:102");
+            ImmutableGameSeason gameSeason3 = TestHelper.AddGameSeason(applicationContext, user3);
+            ImmutableLeaderboard leaderboard3 = TestHelper.AddLeaderboard(applicationContext, gameSeason3);
+
+            ImmutableUser user4 = TestHelper.AddUser(applicationContext, string.Empty, "[::1]:102");
+            ImmutableGameSeason gameSeason4 = TestHelper.AddGameSeason(applicationContext, user4);
+            ImmutableLeaderboard leaderboard4 = TestHelper.AddLeaderboard(applicationContext, gameSeason4);
+
+
+            GameSeasonBL gameSeasonBL = new GameSeasonBL(applicationContext);
+            LeaderboadBL leaderboadBL = new LeaderboadBL(applicationContext);
+            IReadOnlyList<ImmutableLeaderboard> leaderBoardEntries = leaderboadBL.Get(3);
+            Assert.Multiple(() =>
+            {
+                Assert.That(leaderBoardEntries, Has.Count.EqualTo(3));
+
+                Assert.That(gameSeasonBL.Get(leaderBoardEntries[0].GameSeasonId).UserId, Is.EqualTo(user1.Id));
+                Assert.That(gameSeasonBL.Get(leaderBoardEntries[1].GameSeasonId).UserId, Is.EqualTo(user2.Id));
+                Assert.That(gameSeasonBL.Get(leaderBoardEntries[2].GameSeasonId).UserId, Is.EqualTo(user3.Id));
+
+                Assert.That(leaderBoardEntries[0].Rank, Is.EqualTo(1));
+                Assert.That(leaderBoardEntries[1].Rank, Is.EqualTo(2));
+                Assert.That(leaderBoardEntries[2].Rank, Is.EqualTo(3));
+            });
+        }
+
+        [Test]
         public void AddEntriesThatHasSameScoreTest()
         {
             ApplicationContext applicationContext = new ApplicationContext();
